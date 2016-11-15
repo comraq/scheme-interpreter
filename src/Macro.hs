765091e25343ff77949eb1de4ba825b4605b00c3
comraq <yinadam@hotmail.com>
2016-11-15 12:09:53 -0800

    Refactoring to use StateT to Match Patterns

--- src/Macro.hs
+++ src/Macro.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections
+           , GeneralizedNewtypeDeriving
+  #-}
 
 module Macro
   ( expandMacro
@@ -6,8 +8,9 @@ module Macro
   ) where
 
 import Control.Monad.Except
+import Control.Monad.State
 import Control.Monad.Trans.Maybe
-import Data.Foldable (foldrM)
+import Data.Foldable (foldrM, fold)
 
 import Definition
 import LispVector (vectorLength, vectorToList)
@@ -64,6 +67,109 @@ validateRule val@(LList [pat, expr]) = case pat of
   where badSyntaxErr :: LispError
         badSyntaxErr = BadSpecialForm "Invalid 'syntax-rules'" val
 validateRule val = throwError $ BadSpecialForm "Invalid 'syntax-rules'" val
+
+type Bindings = [(VarName, LispVal)]
+type Renames  = [(VarName, VarName)]
+type Ellipsis = [VarName]
+
+type SyntaxEnv = (Env, Renames, Ellipsis)
+
+newtype SEnv = SEnv (Bindings, Ellipsis)
+  deriving Monoid
+
+type PatLispVal   = LispVal
+type InputLispVal = LispVal
+
+makeSyntaxEnv :: LispVal -> Env -> [String] -> SyntaxRule -> MaybeT IO SyntaxEnv
+makeSyntaxEnv val env literals (pat, template) = do
+    pat'                      <- liftMaybe $ dropSynId pat
+    SEnv (bindings, ellipsis) <- liftMaybe $ matchPat (val, pat')
+    (newEnv, renames)         <- liftIO $ bindMacroEnv bindings
+    return (newEnv, renames, ellipsis)
+
+  where
+    bindMacroEnv :: [(VarName, LispVal)] -> IO (Env, [(VarName, VarName)])
+    bindMacroEnv = foldrM (\(name, val) (env, renames) -> do
+      bound <- isBound name env
+      if bound
+        then let newname = '0':name
+             in (, (name, newname):renames) <$> (env `bindVars` [(newname, val)])
+        else (, renames) <$> (env `bindVars` [(name, val)]))
+      (env, [])
+
+    matchPat' :: PatLispVal -> StateT InputLispVal Maybe SEnv
+    matchPat' (LAtom lit) = do
+      inF <- get
+      lift $ if lit `elem` literals
+               then case inF of
+                      LAtom a | a == lit  -> Just mempty
+                              | otherwise -> Nothing
+                      _ -> Nothing
+               else Just $ SEnv ([(lit, inF)], [])
+
+    matchPat' (LList pats) = do
+      inF <- get
+      case (inF, pats) of
+        (LList [], [])               -> lift $ Just mempty
+        (LList vs, p:LAtom "...":ps) -> undefined
+        (LList (v:vs), p:ps)         -> do
+          senv <- lift $ evalStateT (matchPat' p) v
+          put $ LList vs
+          mappend senv <$> matchPat' (LList ps)
+        _ -> lift Nothing
+
+    matchPat' _ = undefined
+
+    matchPat :: (LispVal, LispVal) -> Maybe SEnv
+    matchPat (inF, LAtom lit)
+        -- pattern expects syntax literal
+      | lit `elem` literals = case inF of
+          LAtom a | a == lit  -> return mempty
+                  | otherwise -> Nothing
+          _ -> Nothing
+        -- bind pattern as binding for value in environment
+      | otherwise           = return $ SEnv ([(lit, inF)], [])
+
+{-
+    matchPat (LList vs,         LList ps)
+      | LAtom "..." `elem` ps  =
+          let (tempPs, _:rest) = break (== LAtom "...") ps
+              lenPs'       = length tempPs - 1
+              (ps', eid)   = splitAt lenPs' tempPs
+              (vs', vs'')  = splitAt lenPs' vs
+          in  if length vs' < lenPs'
+                then Nothing
+                else undefined
+
+      | length vs /= length ps = Nothing
+      | otherwise              = fmap fold . traverse matchPat $ zip vs ps
+      -- | otherwise              = fmap join . traverse matchPat $ zip vs ps
+
+    matchPat (LList vs,         LDottedList ps p)
+      | length vs < length ps = Nothing
+      | otherwise             =
+          let (vs', vals) = splitAt (length ps) vs
+          in  (++) <$> matchPat (LList vals, p)
+                   <*> fmap join (traverse matchPat $ zip vs' ps)
+
+    matchPat (LDottedList vs v, LDottedList ps p)
+      | length vs /= length ps = Nothing
+      | otherwise              = (++) <$> matchPat (v, p)
+                                      <*> fmap join (traverse matchPat $ zip vs ps)
+
+    matchPat (LVector vec,      LVector pVec)
+      | vectorLength vec /= vectorLength pVec = Nothing
+      | otherwise                             =
+            fmap join . traverse matchPat
+          $ zip (vectorToList vec) (vectorToList pVec)
+          -}
+
+    matchPat _ = Nothing
+
+    dropSynId :: LispVal -> Maybe LispVal
+    dropSynId (LList (_:ps))         = Just $ LList ps
+    dropSynId (LDottedList (_:ps) p) = Just $ LDottedList ps p
+    dropSynId _                      = Nothing
 
 matchPattern :: LispVal -> Env -> [String] -> SyntaxRule -> MaybeT IOEvaled LispVal
 matchPattern val env literals (pat, template) = do
